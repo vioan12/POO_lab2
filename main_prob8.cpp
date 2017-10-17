bec3847f9e328db417cf6dad25f81064ab596a6c
Ioan-Valentin Voiculescu <vivoicu12@gmail.com>
2017-10-17 11:37:59 +0300

    Update main_prob8.cpp

--- main_prob8.cpp
+++ main_prob8.cpp
@@ -3,22 +3,28 @@
 int main()
 {
 	int x1[50], x2[50], x3[50], x4[50];
-	generare_n();
-	generare(x1);
-	afisare(x1);
-	generare(x2);
-	afisare(x2);
-	generare(x3);
-	afisare(x3);
-	generare(x4);
-	afisare(x4);
-	add(x1, x2);
-	afisare(vect);
-	reset_vector(vect);
-	add(x1, x2,x3);
-	afisare(vect);
-	reset_vector(vect);
-	add(x1, x2, x3,x4);
-	afisare(vect);
+	int* vect;
+	int n;
+
+	n = generare_n();
+
+	vect = new int[n];
+
+	generare(n, x1);
+	afisare(n, x1);
+	generare(n, x2);
+	afisare(n, x2);
+	generare(n, x3);
+	afisare(n, x3);
+	generare(n, x4);
+	afisare(n, x4);
+	vect = add(n, x1, x2);
+
+	vect = add(n, x1, x2,x3);
+	reset_vector(n, vect);
+	vect = add(n, x1, x2, x3,x4);
+	afisare(n, vect);
+
+	delete[] vect;
 	return 0;
 }
